2a78d623b880511144600770bbe4b9cbcf96b288
daisuke <dsaito85a@gmail.com>
2019-10-01 13:37:55 +0900

    web.phpでクローリングをできる範囲で実施

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,37 @@
 */
 
 Route::get('/', function () {
-    $crawler = Goutte::request('GET', 'https://duckduckgo.com/html/?q=Laravel');
-    $crawler->filter('.result__title .result__a')->each(function ($node) {
+    $crawler = Goutte::request('GET', 'http://funspot.jugem.jp/');
+
+    // dump("start");
+    // dump($crawler->filter('.entry')->children()->text());
+    // dump("end");
+
+    dump("start");
+    $crawler->filter('.entry')->each(function ($node) {
+        // dump($node->children()->text());
+        $title = $node->filter('.entry-title a')->text();
+        $text = "";
+        // dump($node->filter('.jgm_entry_desc_mark p')->text());
+        $node->filter('.jgm_entry_desc_mark p')->each(function ($node) {
+            dump($node->text());
+            // var_dump($node->text());
+            // $text = $node->text();
+            // // $text .= $node->text();
+        });
+
+        dump($title);
+        dump($text);
+    });
+    dump("end");
+
+    $crawler->filter('.entry-title a')->each(function ($node) {
         dump($node->text());
     });
+
+    $crawler->filter('.jgm_entry_desc_mark a')->each(function ($node) {
+        dump($node->text());
+    });
+
     return view('welcome');
 });
