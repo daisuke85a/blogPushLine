906285a1431d853275a73bd2eea0f81f30b349af
daisuke <dsaito85a@gmail.com>
2019-10-01 14:07:56 +0900

    無駄なテキストの削除

--- app/Http/Controllers/ScrapingController.php
+++ app/Http/Controllers/ScrapingController.php
@@ -19,15 +19,10 @@ class ScrapingController extends Controller
 
         dump("start");
         $crawler->filter('.entry')->each(function ($node) {
-            // dump($node->children()->text());
             $title = $node->filter('.entry-title a')->text();
             $this->text = "";
-            // dump($node->filter('.jgm_entry_desc_mark p')->text());
             $node->filter('.jgm_entry_desc_mark p')->each(function ($node) {
-                // dump($node->text());
-                // var_dump($node->text());
                 $this->text =  $this->text . $node->text();
-                // // $text .= $node->text();
             });
 
             dump($title);
