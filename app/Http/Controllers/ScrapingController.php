556e0d9a40e2f294a84da97026c3d0216c58700c
daisuke <dsaito85a@gmail.com>
2019-10-01 14:03:26 +0900

    タイトルと本文のクローリングに成功

--- app/Http/Controllers/ScrapingController.php
+++ app/Http/Controllers/ScrapingController.php
@@ -7,6 +7,8 @@ use Goutte;
 
 class ScrapingController extends Controller
 {
+    private $text;
+
     public function scraping(Request $request)
     {
         $crawler = Goutte::request('GET', 'http://funspot.jugem.jp/');
@@ -19,27 +21,19 @@ class ScrapingController extends Controller
         $crawler->filter('.entry')->each(function ($node) {
             // dump($node->children()->text());
             $title = $node->filter('.entry-title a')->text();
-            $text = "";
+            $this->text = "";
             // dump($node->filter('.jgm_entry_desc_mark p')->text());
             $node->filter('.jgm_entry_desc_mark p')->each(function ($node) {
-                dump($node->text());
+                // dump($node->text());
                 // var_dump($node->text());
-            // $text = $node->text();
-            // // $text .= $node->text();
+                $this->text =  $this->text . $node->text();
+                // // $text .= $node->text();
             });
 
             dump($title);
-            dump($text);
+            dump($this->text);
         });
         dump("end");
-
-        $crawler->filter('.entry-title a')->each(function ($node) {
-            dump($node->text());
-        });
-
-        $crawler->filter('.jgm_entry_desc_mark a')->each(function ($node) {
-            dump($node->text());
-        });
 
         return view('welcome');
     }
