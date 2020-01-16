ccfc06fdee9a41c76c04174c2943e45bb838055b
daisuke saito <dsaito85a@gmail.com>
2020-01-16 19:58:08 +0900

    mod: 毎朝11時に実行するよう修正

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('blog:check')->everyMinute();
+        $schedule->command('blog:check')->dailyAt('11:00');
     }
 
     /**
